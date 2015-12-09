d82d001c607921c5adbf0388aee7eb715507dad9
xiaorui <zhkuliqinrui@163.com>
2015-12-09 20:58:31 +0800

    完成版

--- app/src/main/java/com/example/administrator/androidltest/Activity1.java
+++ app/src/main/java/com/example/administrator/androidltest/Activity1.java
@@ -2,6 +2,9 @@ package com.example.administrator.androidltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.Logger;
 
 public class Activity1 extends AppCompatActivity {
 
@@ -9,7 +12,19 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+
     }
+
+    private void stepNext(int i){
+
+        Log.i("Activity1","stepNext当前的i的值：" + i);
+
+
+    }
+
+
+
 
 
 
